ac4560a2479a97c98453f00221c49e9dac10b906
Christopher_Madrid <Chris_Madrid@baylor.edu>
2017-11-03 15:57:51 +0100

    Making more plots

--- CU_Plots.C
+++ CU_Plots.C
@@ -19,9 +19,9 @@
 
 //Data.....
 //{CU,RBX,Run,pd_ch,uhtr_ch,shunt,max_adc,max_fc,result} 
-#include "pd_array.h"
+#include "pd_array_test.h"
 //{CU,RBX,Run,RM,sipm_ch,uhtr_ch,shunt,max_adc,max_fc,result}
-#include "sipm_array.h"
+#include "sipm_array_test.h"
 
 void CU_Plots(){
   int NumChanPD = pd_array.size();
@@ -34,12 +34,12 @@ void CU_Plots(){
   //----------------------------------------------------------------------
   Double_t xPD0[NumChanPD],xPD1[NumChanPD],xPD2[NumChanPD],xPD3[NumChanPD],xPD4[NumChanPD],xPD5[NumChanPD];
   Double_t yPD0[NumChanPD],yPD1[NumChanPD],yPD2[NumChanPD],yPD3[NumChanPD],yPD4[NumChanPD],yPD5[NumChanPD];
-  TProfile* profile_pd0 = new TProfile("pd0","pd0",45,0,44,0,350000);
-  TProfile* profile_pd1 = new TProfile("pd1","pd1",45,0,44,0,350000);
-  TProfile* profile_pd2 = new TProfile("pd2","pd2",45,0,44,0,350000);
-  TProfile* profile_pd3 = new TProfile("pd3","pd3",45,0,44,0,350000);
-  TProfile* profile_pd4 = new TProfile("pd4","pd4",45,0,44,0,350000);
-  TProfile* profile_pd5 = new TProfile("pd5","pd5",45,0,44,0,350000);
+  TProfile* profile_pd0 = new TProfile("pd0","pd0",44,-0.5,43.5,0,350000);
+  TProfile* profile_pd1 = new TProfile("pd1","pd1",44,-0.5,43.5,0,350000);
+  TProfile* profile_pd2 = new TProfile("pd2","pd2",44,-0.5,43.5,0,350000);
+  TProfile* profile_pd3 = new TProfile("pd3","pd3",44,-0.5,43.5,0,350000);
+  TProfile* profile_pd4 = new TProfile("pd4","pd4",44,-0.5,43.5,0,350000);
+  TProfile* profile_pd5 = new TProfile("pd5","pd5",44,-0.5,43.5,0,350000);
   TH1F* allPD_dist = new TH1F("allPD","allPD",62,0,350000);
   TH1F* PD0_dist   = new TH1F("PD0","PD0",62,0,350000);
   TH1F* PD1_dist   = new TH1F("PD1","PD1",62,0,350000);
@@ -102,12 +102,12 @@ void CU_Plots(){
   TGraph* graph_pd3 = new TGraph(NumChanPD,xPD3,yPD3);
   TGraph* graph_pd4 = new TGraph(NumChanPD,xPD4,yPD4);
   TGraph* graph_pd5 = new TGraph(NumChanPD,xPD5,yPD5);
-  graph_pd0->SetMarkerColor(kBlack);
-  graph_pd1->SetMarkerColor(kRed);
-  graph_pd2->SetMarkerColor(kBlue);
-  graph_pd3->SetMarkerColor(kGreen+2);
-  graph_pd4->SetMarkerColor(kOrange);
-  graph_pd5->SetMarkerColor(kMagenta);      
+  graph_pd0->SetMarkerColor(kMagenta);     profile_pd0->SetLineColor(kMagenta);
+  graph_pd1->SetMarkerColor(kOrange);      profile_pd1->SetLineColor(kOrange);
+  graph_pd2->SetMarkerColor(kBlue+2);      profile_pd2->SetLineColor(kBlue+2);
+  graph_pd3->SetMarkerColor(kGray);        profile_pd3->SetLineColor(kGray);
+  graph_pd4->SetMarkerColor(kYellow+2);    profile_pd4->SetLineColor(kYellow+2);
+  graph_pd5->SetMarkerColor(kMagenta+2);   profile_pd5->SetLineColor(kMagenta+2);      
   graph_pd0->SetMarkerStyle(kFullSquare);
   graph_pd1->SetMarkerStyle(kFullSquare);
   graph_pd2->SetMarkerStyle(kFullSquare);
@@ -120,10 +120,10 @@ void CU_Plots(){
   //----------------------------------------------------------------------
   Double_t xRM1[NumChanRM],xRM2[NumChanRM],xRM3[NumChanRM],xRM4[NumChanRM];
   Double_t yRM1[NumChanRM],yRM2[NumChanRM],yRM3[NumChanRM],yRM4[NumChanRM];
-  TProfile* profile_rm1 = new TProfile("RM1","RM1",45,0,44,0,350000);
-  TProfile* profile_rm2 = new TProfile("RM2","RM2",45,0,44,0,350000);
-  TProfile* profile_rm3 = new TProfile("RM3","RM3",45,0,44,0,350000);
-  TProfile* profile_rm4 = new TProfile("RM4","RM4",45,0,44,0,350000);  
+  TProfile* profile_rm1 = new TProfile("RM1","RM1",44,-0.5,43.5,0,350000);
+  TProfile* profile_rm2 = new TProfile("RM2","RM2",44,-0.5,43.5,0,350000);
+  TProfile* profile_rm3 = new TProfile("RM3","RM3",44,-0.5,43.5,0,350000);
+  TProfile* profile_rm4 = new TProfile("RM4","RM4",44,-0.5,43.5,0,350000);  
   TH1F* allRM_dist = new TH1F("allRM","allRM",62,0,350000);
   TH1F* RM1_dist   = new TH1F("RM1 Dist","RM1 Dist",62,0,350000);
   TH1F* RM2_dist   = new TH1F("RM2 Dist","RM2 Dist",62,0,350000);
@@ -165,10 +165,10 @@ void CU_Plots(){
   TGraph* graph_rm2 = new TGraph(NumChanRM,xRM2,yRM2);
   TGraph* graph_rm3 = new TGraph(NumChanRM,xRM3,yRM3);
   TGraph* graph_rm4 = new TGraph(NumChanRM,xRM4,yRM4);
-  graph_rm1->SetMarkerColor(kRed);
-  graph_rm2->SetMarkerColor(kBlue);
-  graph_rm3->SetMarkerColor(kGreen+2);
-  graph_rm4->SetMarkerColor(kBlack);
+  graph_rm1->SetMarkerColor(kRed);        profile_rm1->SetLineColor(kRed);
+  graph_rm2->SetMarkerColor(kBlue);       profile_rm2->SetLineColor(kBlue);
+  graph_rm3->SetMarkerColor(kBlack);      profile_rm3->SetLineColor(kBlack);
+  graph_rm4->SetMarkerColor(kGreen+2);    profile_rm4->SetLineColor(kGreen+2);
   graph_rm1->SetMarkerStyle(kFullSquare);
   graph_rm2->SetMarkerStyle(kFullSquare);
   graph_rm3->SetMarkerStyle(kFullSquare);
@@ -417,45 +417,142 @@ void CU_Plots(){
   Mean3->Draw();
   Stddev3->Draw();
   RM->Draw();
+
+  TCanvas *c4 = new TCanvas("MaxfCvsCU 0,1, RM1-4","",800,800);  
+  TLegend* catLeg4 = new TLegend(0.68,0.65,0.96,0.88);
+  catLeg4->SetBorderSize(0);
+  catLeg4->SetFillStyle(0);
+  catLeg4->SetTextSize(0.04);
+  gPad->SetTopMargin(0.1);
+  gPad->SetBottomMargin(0.12);
+  gPad->SetRightMargin(0.05);
+  gPad->SetLeftMargin(0.14);  
+  //gPad->SetLogy();
   
-  TCanvas *c4  = new TCanvas("1D PD0","",800,800);
+  TH1F* h4blank = new TH1F("Blank4","",250,0,50);
+  h4blank->SetMinimum(0);
+  h4blank->SetMaximum(350000);
+  //h4blank->GetXaxis()->SetRange(0,100);
+  h4blank->GetXaxis()->SetTitle("CU");
+  h4blank->GetYaxis()->SetTitle("Max Charge [fC]");
+  h4blank->SetTitle("");
+  h4blank->SetName("");
+  h4blank->SetTitleSize(0.002);
+  h4blank->SetTitleSize(0.05,"X");
+  h4blank->SetTitleSize(0.05,"Y");
+  h4blank->SetTitleOffset(1.0,"X");
+  h4blank->SetTitleOffset(1.2,"Y");
+  h4blank->SetLabelSize(0.04,"X");
+  h4blank->SetLabelSize(0.04,"Y");
+  h4blank->SetStats(false);
+  h4blank->SetLineColor(0);
+  h4blank->Draw("hist");
+  
+  graph_pd0->Draw("P same");
+  graph_pd1->Draw("P same");
+  graph_rm1->Draw("P same");
+  graph_rm2->Draw("P same");
+  graph_rm3->Draw("P same");
+  graph_rm4->Draw("P same");
+  catLeg4->AddEntry(graph_pd0,"Pin-Diode 0","P");
+  catLeg4->AddEntry(graph_pd1,"Pin-Diode 1","P");
+  catLeg4->AddEntry(graph_rm1,"RM 1","P");
+  catLeg4->AddEntry(graph_rm2,"RM 2","P");
+  catLeg4->AddEntry(graph_rm3,"RM 3","P");
+  catLeg4->AddEntry(graph_rm4,"RM 4","P");
+
+  catLeg4->Draw();  
+  CMSPrelim1->Draw();
+  burnIn->Draw();
+
+  TCanvas *c5 = new TCanvas("Profile: MaxfCvsCU 0,1, RM1-4","",800,800);  
+  TLegend* catLeg5 = new TLegend(0.68,0.65,0.96,0.88);
+  catLeg5->SetBorderSize(0);
+  catLeg5->SetFillStyle(0);
+  catLeg5->SetTextSize(0.04);
+  gPad->SetTopMargin(0.1);
+  gPad->SetBottomMargin(0.12);
+  gPad->SetRightMargin(0.05);
+  gPad->SetLeftMargin(0.14);  
+  //gPad->SetLogy();
+  
+  TH1F* h5blank = new TH1F("Blank5","",250,0,50);
+  h5blank->SetMinimum(0);
+  h5blank->SetMaximum(350000);
+  //h5blank->GetXaxis()->SetRange(0,100);
+  h5blank->GetXaxis()->SetTitle("CU");
+  h5blank->GetYaxis()->SetTitle("Max Charge [fC]");
+  h5blank->SetTitle("");
+  h5blank->SetName("");
+  h5blank->SetTitleSize(0.002);
+  h5blank->SetTitleSize(0.05,"X");
+  h5blank->SetTitleSize(0.05,"Y");
+  h5blank->SetTitleOffset(1.0,"X");
+  h5blank->SetTitleOffset(1.2,"Y");
+  h5blank->SetLabelSize(0.04,"X");
+  h5blank->SetLabelSize(0.04,"Y");
+  h5blank->SetStats(false);
+  h5blank->SetLineColor(0);
+  h5blank->Draw("hist");
+  
+  profile_pd0->Draw("P same");
+  profile_pd1->Draw("P same");
+  profile_rm1->Draw("P same");
+  profile_rm2->Draw("P same");
+  profile_rm3->Draw("P same");
+  profile_rm4->Draw("P same");
+  catLeg5->AddEntry(profile_pd0,"Pin-Diode 0","P");
+  catLeg5->AddEntry(profile_pd1,"Pin-Diode 1","P");
+  catLeg5->AddEntry(profile_rm1,"RM 1","P");
+  catLeg5->AddEntry(profile_rm2,"RM 2","P");
+  catLeg5->AddEntry(profile_rm3,"RM 3","P");
+  catLeg5->AddEntry(profile_rm4,"RM 4","P");
+
+  catLeg4->Draw();  
+  CMSPrelim1->Draw();
+  burnIn->Draw();
+
+  TCanvas *d5  = new TCanvas("1D PD0","",800,800);
   PD0_dist->Draw();  
-  TCanvas *c5  = new TCanvas("1D PD1","",800,800);
+  TCanvas *d6  = new TCanvas("1D PD1","",800,800);
   PD1_dist->Draw();  
-  TCanvas *c6  = new TCanvas("1D PD2","",800,800);
+  TCanvas *d7  = new TCanvas("1D PD2","",800,800);
   PD2_dist->Draw();  
-  TCanvas *c7  = new TCanvas("1D PD3","",800,800);
+  TCanvas *d8  = new TCanvas("1D PD3","",800,800);
   PD3_dist->Draw();  
-  TCanvas *c8  = new TCanvas("1D PD4","",800,800);
+  TCanvas *d9  = new TCanvas("1D PD4","",800,800);
   PD4_dist->Draw();  
-  TCanvas *c9  = new TCanvas("1D PD5","",800,800);
+  TCanvas *d10  = new TCanvas("1D PD5","",800,800);
   PD5_dist->Draw();  
-  TCanvas *c10 = new TCanvas("1D PD2 3 4 5","",800,800);
+  TCanvas *d11 = new TCanvas("1D PD2 3 4 5","",800,800);
   PD2_3_4_5_dist->Draw();
-  TCanvas *c11 = new TCanvas("1D RM1","",800,800);
+  TCanvas *d12 = new TCanvas("1D RM1","",800,800);
   RM1_dist->Draw();
-  TCanvas *c12 = new TCanvas("1D RM2","",800,800);
+  TCanvas *d13 = new TCanvas("1D RM2","",800,800);
   RM2_dist->Draw();
-  TCanvas *c13 = new TCanvas("1D RM3","",800,800);
+  TCanvas *d14 = new TCanvas("1D RM3","",800,800);
   RM3_dist->Draw();
-  TCanvas *c14 = new TCanvas("1D RM4","",800,800);
+  TCanvas *d15 = new TCanvas("1D RM4","",800,800);
   RM4_dist->Draw();
 
   c0->SaveAs("PDAllvsCU2D.pdf");
   c1->SaveAs("PDAll_1D.pdf");
   c2->SaveAs("RMAllvsCU2D.pdf");
   c3->SaveAs("RMAll_1D.pdf");
-  c4->SaveAs("PD0_1D.pdf");
-  c5->SaveAs("PD1_1D.pdf");
-  c6->SaveAs("PD2_1D.pdf");
-  c7->SaveAs("PD3_1D.pdf");
-  c8->SaveAs("PD4_1D.pdf");
-  c9->SaveAs("PD5_1D.pdf");
-  c10->SaveAs("PD2_3_4_5_1D.pdf");
-  c11->SaveAs("RM1_1D.pdf");
-  c12->SaveAs("RM2_1D.pdf");
-  c13->SaveAs("RM3_1D.pdf");
-  c14->SaveAs("RM4_1D.pdf");
+  c4->SaveAs("RM_PDvsCU2D.pdf");
+  c5->SaveAs("profile_RM_PDvsCU2D.pdf");
+  
+  d5->SaveAs("PD0_1D.pdf");
+  d6->SaveAs("PD1_1D.pdf");
+  d7->SaveAs("PD2_1D.pdf");
+  d8->SaveAs("PD3_1D.pdf");
+  d9->SaveAs("PD4_1D.pdf");
+  d10->SaveAs("PD5_1D.pdf");
+  d11->SaveAs("PD2_3_4_5_1D.pdf");
+  d12->SaveAs("RM1_1D.pdf");
+  d13->SaveAs("RM2_1D.pdf");
+  d14->SaveAs("RM3_1D.pdf");
+  d15->SaveAs("RM4_1D.pdf");
 } 
 
 int main(){
