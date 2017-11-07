2d442576d2cf82844292f559c2fbb3cf74614d45
Caleb James Smith <caleb_smith2@baylor.edu>
2017-11-07 01:22:21 +0100

    add sipm vs pd plots

--- plotter.py
+++ plotter.py
@@ -10,26 +10,27 @@ def getData(data_dir):
     pd_file = "pd_table.txt"
     dictionaries = [ { "file" : sipm_file, "tag" : "sipm", "element" : "rm" },
                      { "file" : pd_file,   "tag" : "pd",   "element" : "pd" } ]
+    
     data = {}
     
     data["sipm"] = []
-    data["rm1"] = []
-    data["rm2"] = []
-    data["rm3"] = []
-    data["rm4"] = []
-    
     data["pd"] = []
-    data["pd0"] = []
-    data["pd1"] = []
-    data["pd2"] = []
-    data["pd3"] = []
-    data["pd4"] = []
-    data["pd5"] = []
+    
     data["pd_quartz"] = []
     data["pd_megatile"] = []
     
+    for pd in xrange(6):
+        element_name = "pd%d" % pd
+        data[element_name] = []
+        data["%s_ave" % element_name] = []
+    for rm in xrange(1,5):
+        element_name = "rm%d" % rm
+        data[element_name] = []
+        data["%s_ave" % element_name] = []
+
     # sipm array: {CU,RBX,Run,RM,sipm_ch,uhtr_ch,shunt,max_adc,max_fc,result}
     # pd array: {CU,RBX,Run,pd_ch,uhtr_ch,shunt,max_adc,max_fc,result}
+    cu_list = []
     for dictionary in dictionaries:
         f = dictionary["file"]
         tag = dictionary["tag"]
@@ -41,18 +42,64 @@ def getData(data_dir):
                 if line[0] == "#":
                     continue
                 s = line.split()
-                if int(s[-1]) == 0:
+                #if int(s[-1]) == 0:
                     # skip data that does not pass
                     #print line
-                    continue
+                    #continue
                 d = float(s[fc_position])
-                element_value = int(s[element_position])
-                element_name = "%s%d" % (element, element_value)
+                element_index = int(s[element_position])
+                element_name = "%s%d" % (element, element_index)
                 data[tag].append(d)
                 data[element_name].append(d)
+                
+                # data organized by CU
+                cu = int(s[0]) 
+                if cu not in cu_list:
+                    cu_list.append(cu) 
+                    cu_name = "cu%d" % cu
+                    data[cu_name] = {}
+                    for pd in xrange(6):
+                        data[cu_name]["pd%d" % pd] = []
+                    for rm in xrange(1,5):
+                        data[cu_name]["rm%d" % rm] = []
+
+                data["cu%d"%cu][element_name].append(d)
    
     data["pd_quartz"] = data["pd0"] + data["pd0"]
     data["pd_megatile"] = data["pd2"] + data["pd3"] + data["pd4"] + data["pd5"]
+    
+    for cu in cu_list:
+        cu_key = "cu%d" % cu
+        for pd in xrange(6):
+            element_key ="pd%d" % pd 
+            ave = np.mean(data[cu_key][element_key])
+            data["%s_ave" % element_key].append(ave)
+        for rm in xrange(1,5):
+            element_key ="rm%d" % rm
+            ave = np.mean(data[cu_key][element_key])
+            data["%s_ave" % element_key].append(ave)
+    
+    # number of channels for rm 1, 2, 3, 4
+    '''
+    num_channels = [48, 46, 46, 48]
+    total_channels = sum(num_channels)
+    for rm in xrange(1,5):
+        start, i = 0, 1
+        while i < rm:
+            start += num_channels[i-1]
+            i += 1
+        rm_data = data["rm%d"%rm] 
+        rm_channels = num_channels[rm-1]
+        data["rm%d_ave"%rm] = []
+        j = start
+        while j < len(rm_data):
+            rm_ave = np.mean(rm_data[j:j+rm_channels])
+            data["rm%d_ave"%rm].append(rm_ave) 
+            j += total_channels
+    '''
+
+    #for key in data:
+    #    print "length of {0} data: {1}".format(key, len(data[key]))
 
     return data
 
@@ -83,18 +130,80 @@ def plotHisto(data, plot_dir, info):
     plt.savefig(plot_dir + name + ".pdf")
     plt.clf()
 
-def plotScatter(x_list, y_list, plot_dir, info):
+def plotScatter(x, y, plot_dir, info):
     name = info["name"]
     title = info["title"]
     xtitle = info["xtitle"]
     ytitle = info["ytitle"]
-    plt.scatter(x_list, y_list)
+    xstat = info["xstat"]
+    ystat = info["ystat"]
+    
+    # calculate fit function
+    z = np.polyfit(x, y, 1)
+    f = np.poly1d(z)
+    f_string = str(f)
+    f_string = f_string.split("\n")[-1]
+    f_string = "f(x) = {0}".format(f_string)
+    print f_string
+
+    # calculate new x's and y's using fit function
+    x_new = np.linspace(min(x), max(x), 50)
+    y_new = f(x_new)
+
+    plt.plot(x,y,'o', x_new, y_new)
+    plt.text(xstat, ystat, f_string)
     plt.title(title)
     plt.xlabel(xtitle)
     plt.ylabel(ytitle)
     plt.savefig(plot_dir + name + ".png")
     plt.savefig(plot_dir + name + ".pdf")
     plt.clf()
+
+def plotRMvsPD(data, plot_dir, info):
+    for pd in xrange(2):
+        name = info["name"]
+        title = info["title"]
+        xtitle = info["xtitle"]
+        ytitle = info["ytitle"]
+        xstat = info["xstat"]
+        ystat = info["ystat"]
+        f_box = ""
+        
+        x = data["pd%d_ave"%pd] 
+        print "number of pd %d ave: %d" % (pd, len(x))
+        print "x = {0}".format(x)
+        
+        for rm in xrange(1,5):
+            
+            y = data["rm%d_ave"%rm] 
+            print "number of rm %d ave: %d" % (rm, len(y))
+            print "y = {0}".format(y)
+            
+            # calculate fit function
+            z = np.polyfit(x, y, 1)
+            f = np.poly1d(z)
+            f_string = str(f)
+            f_string = f_string.split("\n")[-1]
+            f_string = "RM {0}: f(x) = {1}".format(rm, f_string)
+            f_box += f_string + "\n"
+            print f_string
+
+            # calculate new x's and y's using fit function
+            x_new = np.linspace(min(x), max(x), 50)
+            y_new = f(x_new)
+
+            plt.plot(x,y,'o', x_new, y_new)
+        
+        plt.text(xstat, ystat, f_box)
+        
+        title += " %d" % pd
+        xtitle += " %d Max fC" % pd
+        plt.title(title)
+        plt.xlabel(xtitle)
+        plt.ylabel(ytitle)
+        plt.savefig(plot_dir + name + "_pd%d.png" % pd)
+        plt.savefig(plot_dir + name + "_pd%d.pdf" % pd)
+        plt.clf()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -120,14 +229,28 @@ if __name__ == "__main__":
     for key in info:
         plotHisto(data, plot_dir, info[key])
 
-    pd1vspd0_info = {}
-    pd1vspd0_info["name"] = "pd1_pd0"
-    pd1vspd0_info["title"] = "Pin Diode 1 vs Pin Diode 0"
-    pd1vspd0_info["xtitle"] = "Pin Diode 0 Max fC"
-    pd1vspd0_info["ytitle"] = "Pin Diode 1 Max fC"
+    # PD1 vs PD0
+    pd1_pd0_info = {}
+    pd1_pd0_info["name"] = "pd1_pd0"
+    pd1_pd0_info["title"] = "Pin Diode 1 vs Pin Diode 0"
+    pd1_pd0_info["xtitle"] = "Pin Diode 0 Max fC"
+    pd1_pd0_info["ytitle"] = "Pin Diode 1 Max fC"
+    pd1_pd0_info["xstat"] = 20000 
+    pd1_pd0_info["ystat"] = 50000
 
-    x_list = data["pd0"]
-    y_list = data["pd1"]
-    plotScatter(x_list, y_list, plot_dir, pd1vspd0_info)
+    # RM1-4 vs PD0 and PD1
+    rm_pd_info = {}
+    rm_pd_info["name"] = "rm_pd"
+    rm_pd_info["title"] = "SiPMs vs Pin Diode"
+    rm_pd_info["xtitle"] = "Pin Diode"
+    rm_pd_info["ytitle"] = "SiPM Max fC"
+    rm_pd_info["xstat"] = 20000 
+    rm_pd_info["ystat"] = 50000
+
+    x = data["pd0"]
+    y = data["pd1"]
+    plotScatter(x, y, plot_dir, pd1_pd0_info)
+
+    plotRMvsPD(data, plot_dir, rm_pd_info)
 
 
