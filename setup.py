b73b5fc5e22b105393b621468383bea2a52cfe39
Caleb James Smith <caleb_smith2@baylor.edu>
2017-11-18 12:27:39 +0100

    data from november 17-18

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import sys
 from sendCommands import *
 from argparse import ArgumentParser
+import time
 
 control_hub = "hcal904daq04"
 
@@ -10,6 +11,7 @@ cmdLists.append([])
 cmdLists.append([])
 cmdLists[0].append("put RBX-[1-4]-QIE[1-48]_Gsel 192*31")
 cmdLists[0].append("put RBX-calib-QIE[1-12]_Gsel 12*31")
+cmdLists[0].append("tput RBX-lg push")
 cmdLists[1].append("get RBX-[1-4]-QIE[1-48]_Gsel_rr")
 cmdLists[1].append("get RBX-calib-QIE[1-12]_Gsel_rr")
 
@@ -26,8 +28,10 @@ def main():
         port = 64100
     
     for cmdList in cmdLists:
+        time.sleep(1)
         commands = list(c.replace("RBX", "HE%d" % rbx) for c in cmdList)
         results = send_commands(cmds=commands,script=True,port=port,control_hub=control_hub)
+        time.sleep(1)
         for result in results:
             print result["cmd"] + " : " + result["result"] 
         
